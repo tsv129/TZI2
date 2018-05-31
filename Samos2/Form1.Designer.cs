adb1b4db8b8bbf2ce3725b70c3d2e7f0596b3062
tsv129 <32687515+tsv129@users.noreply.github.com>
2018-06-01 00:02:07 +0300

    Release

---
+++ Samos2/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Samos2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 45;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(547, 523);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "invalidfunctionresulttype";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(254, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(105, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "45321";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(365, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(105, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "property";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(476, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Crypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(30, 578);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(252, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(30, 604);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(252, 20);
+            this.textBox6.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(472, 578);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(365, 578);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 46);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(325, 633);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(252, 20);
+            this.textBox7.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 676);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "TZI 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ Samos2/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Samos2
+{
+    public partial class Form1 : Form
+    {
+        Char[] Alph = new Char[25] { 'A', 'B', 'C', 'D', 'E',
+            'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        Char[] tmparr = new Char[25];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                dataGridView1.Columns.Add(Alph[i].ToString(), Alph[i].ToString());
+                dataGridView1.Columns[i].Width = 20;
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].HeaderCell.Value = Alph[i].ToString();
+                dataGridView1.Rows[i].Height = 20;
+            }
+            TableCreate(Alph);
+        }
+
+        private void TableCreate(char[] arr)
+        {
+            for (int c = 0; c < 25; c++)
+            {
+                for (int r = 0; r < 25; r++)
+                {
+                    dataGridView1[c, r].Value = arr[(c + r) % 25].ToString();
+                }
+            }
+        }
+
+        private void Shuffle()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    tmparr[i * 5 + j] = Alph[i * 5 + int.Parse(textBox2.Text[j].ToString()) - 1];
+                }
+            }
+        }
+
+        private void Crypt()
+        {
+            int k, n;
+            string str1 = textBox1.Text.ToUpper();
+            string str2 = textBox5.Text.ToUpper();
+            for (int i = 0; i < 25; i++)
+            {
+                for (k = 0; k < 25; k++)
+                {
+                    if (Alph[k] == str1[i])
+                        break;
+                }
+                for (n = 0; n < 25; n++)
+                {
+                    if (Alph[n] == str2[i])
+                        break;
+                }
+                textBox6.Text += dataGridView1[k, n].Value;
+            }
+            textBox5.Text = textBox5.Text.ToUpper();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int len = textBox1.Text.Length;
+            string str = "";
+            for (int i = 0; i < len; i++)
+            {
+                str += textBox3.Text[i % 8];
+            }
+            textBox5.Text = str;
+            Shuffle();
+            TableCreate(tmparr);
+            Crypt();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n, k;
+            string s;
+            for (int i = 0; i < 25; i++)
+            {
+                for (n = 0; n < 25; n++)
+                {
+                    if (textBox5.Text[i] == Alph[n])
+                    {
+                        break;
+                    }
+                }
+                for (k = 0; k < 25; k++)
+                {
+                    s = dataGridView1[n, k].Value.ToString();
+                    if (textBox6.Text[i].ToString() == s)
+                    {
+                        break;
+                    }
+                }
+                textBox7.Text += Alph[k];
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TableCreate(Alph);
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+        }
+    }
+}
